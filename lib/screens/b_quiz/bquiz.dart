b1cb1663e7e1435f2d4fbff7f39e64aff529b86a
NeerajYadu07 <neerajyadu8281@gmail.com>
2022-11-02 01:07:18 +0530

    conflicts resolved

--- lib/screens/b_quiz/bquiz.dart
+++ lib/screens/b_quiz/bquiz.dart
@@ -1,6 +1,9 @@
 import 'package:ecellapp/core/res/colors.dart';
 import 'package:ecellapp/core/res/dimens.dart';
 import 'package:ecellapp/screens/b_quiz/leaderBoard/leaderboard_repository.dart';
+import 'package:ecellapp/screens/b_quiz/cubit/quiz_cubit.dart';
+import 'package:ecellapp/screens/b_quiz/quiz_repository.dart';
+import 'package:ecellapp/screens/b_quiz/quiz_screen.dart';
 import 'package:ecellapp/widgets/gradient_text.dart';
 import 'package:ecellapp/widgets/screen_background.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +11,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'leaderBoard/cubit/leaderboard_cubit.dart';
 import 'leaderBoard/leader_board.dart';
+
 
 import '../../widgets/raisedButton.dart';
 
