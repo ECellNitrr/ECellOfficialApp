7d5e4f0859581f9ad866f09c37e9382c5af7a0ed
Kumar Utsav <utsavkumar.9749@gmail.com>
2022-11-06 11:20:52 +0530

    conflict resolving

--- lib/screens/about_us/about_us.dart
+++ lib/screens/about_us/about_us.dart
@@ -4,7 +4,6 @@ import 'package:ecellapp/screens/about_us/tabs/aim/aim.dart';
 import 'package:ecellapp/screens/about_us/tabs/team/team.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class AboutUsScreen extends StatefulWidget {
   @override
