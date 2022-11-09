76802542943ca50b25c62b708b2a31111736ce55
kvns07 <k.venkatnagsai@gmail.com>
2022-11-09 23:44:52 +0530

    Fixes4

--- lib/screens/about_us/tabs/team/app_team/app_team_card.dart
+++ lib/screens/about_us/tabs/team/app_team/app_team_card.dart
@@ -74,147 +74,149 @@ class AppTeamCard extends StatelessWidget {
   Widget build(BuildContext context) {
     double ratio = MediaQuery.of(context).size.aspectRatio;
     return Container(
-      child: Stack(
-        children: [
-          Container(
-            margin: const EdgeInsets.symmetric(
-                horizontal: D.horizontalPaddingFrame, vertical: 20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(22),
-            ),
-            child: Container(
-              height: ratio > 0.5 ? 150 : 170,
-              margin: EdgeInsets.only(left: 139,right: 10),
+      child: Center(
+        child: Stack(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(
+                  horizontal: D.horizontalPaddingFrame, vertical: 20),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(22),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    appHead!.name!,
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: C.cardFontColor,
-                      fontWeight: FontWeight.w600,
+              child: Container(
+                height: ratio > 0.5 ? 150 : 170,
+                margin: EdgeInsets.only(left: 139,right: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(22),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      appHead!.name!,
+                      style: TextStyle(
+                        fontSize: 23,
+                        color: C.cardFontColor,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 7,),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Expanded(
-                        flex: 3,
-                        child: GestureDetector(
-                          child: RichText(
-                            text: TextSpan(
-                              children: [
-                                WidgetSpan(child: Icon(Icons.phone,size: 16,color: C.blendSocialIconColorTwo,)),
-                                TextSpan(text: appHead!.phone!,
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    color: C.cardFontColor,
-                                    fontWeight: FontWeight.w600,
-                                  ),)
+                    SizedBox(height: 7,),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: GestureDetector(
+                            child: RichText(
+                              text: TextSpan(
+                                children: [
+                                  WidgetSpan(child: Icon(Icons.phone,size: 16,color: C.blendSocialIconColorTwo,)),
+                                  TextSpan(text: appHead!.phone!,
+                                    style: TextStyle(
+                                      fontSize: 13,
+                                      color: C.cardFontColor,
+                                      fontWeight: FontWeight.w600,
+                                    ),)
+                                ],
+                              ),
+                            ),
+                            onTap: () async {
+                              final url = "tel:${appHead!.phone!}";
+                              if (await canLaunchUrlString(url)) {
+                                await launchUrlString(url);
+                              } else {
+                                throw 'Could not launch $url';
+                              }
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: GestureDetector(
+                            onTap: () async {
+                              //Handle sponsor.spons[0].socialMedia
+                              if (await canLaunchUrlString(appHead!.linkedin!)) {
+                                await launchUrlString(appHead!.linkedin!);
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(content: Text(S.redirectIntentError)));
+                              }
+                            },
+                            child: Image.asset(
+                              S.assetIconLinkdin,
+                              height: 21,
+                              color: Color.alphaBlend(
+                                C.blendSocialIconColorOne,
+                                C.blendSocialIconColorTwo,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 7,),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          WidgetSpan(child: Icon(Icons.mail,size: 16,color: C.blendSocialIconColorTwo,)),
+                          WidgetSpan(child: SizedBox(width: 3,)),
+                          TextSpan(text: appHead!.email!,
+                            style: TextStyle(
+                              fontSize: 13,
+                              color: C.cardFontColor,
+                              fontWeight: FontWeight.w600,
+                            ),)
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              height: ratio > 0.5 ? 200 : 220,
+              width: ratio > 0.5 ? 150 : 170,
+              child: Stack(
+                children: [
+                  Image.asset(S.assetSponsorFrame,
+                      fit: BoxFit.cover, height: 220),
+                  Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                            height: 100,
+                            width: 100,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.25),
+                                  offset: Offset(0.0, 5),
+                                  blurRadius: 10,
+                                ),
                               ],
                             ),
-                          ),
-                          onTap: () async {
-                            final url = "tel:${appHead!.phone!}";
-                            if (await canLaunchUrlString(url)) {
-                              await launchUrlString(url);
-                            } else {
-                              throw 'Could not launch $url';
-                            }
-                          },
+                            child: Image.network(appHead!.image!,
+                              fit: BoxFit.contain,
+                              width: 90,
+                              height: 90,)
                         ),
-                      ),
-                      Expanded(
-                        child: GestureDetector(
-                          onTap: () async {
-                            //Handle sponsor.spons[0].socialMedia
-                            if (await canLaunchUrlString(appHead!.linkedin!)) {
-                              await launchUrlString(appHead!.linkedin!);
-                            } else {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  SnackBar(content: Text(S.redirectIntentError)));
-                            }
-                          },
-                          child: Image.asset(
-                            S.assetIconLinkdin,
-                            height: 25,
-                            color: Color.alphaBlend(
-                              C.blendSocialIconColorOne,
-                              C.blendSocialIconColorTwo,
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 7,),
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        WidgetSpan(child: Icon(Icons.mail,size: 16,color: C.blendSocialIconColorTwo,)),
-                        WidgetSpan(child: SizedBox(width: 3,)),
-                        TextSpan(text: appHead!.email!,
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: C.cardFontColor,
-                            fontWeight: FontWeight.w600,
-                          ),)
+                        SizedBox(height: 10,)
                       ],
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-          Positioned(
-            height: ratio > 0.5 ? 200 : 220,
-            width: ratio > 0.5 ? 150 : 170,
-            child: Stack(
-              children: [
-                Image.asset(S.assetSponsorFrame,
-                    fit: BoxFit.cover, height: 220),
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                          height: 100,
-                          width: 100,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.rectangle,
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.25),
-                                offset: Offset(0.0, 5),
-                                blurRadius: 10,
-                              ),
-                            ],
-                          ),
-                          child: Image.network(appHead!.image!,
-                            fit: BoxFit.contain,
-                            width: 90,
-                            height: 90,)
-                      ),
-                      SizedBox(height: 10,)
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
